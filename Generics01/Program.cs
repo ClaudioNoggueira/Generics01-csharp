b4a093d532cbe09c3473398f4291e7b41d0841f3
Claudio Nogueira <cllaudionoggueira@gmail.com>
2020-12-28 01:05:51 -0300

    First commit

---
+++ Generics01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics01 {
+    class Program {
+        static void Main(string[] args) {
+            PrintService<string> print = new PrintService<string>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) {
+                string x = Console.ReadLine();
+                print.AddValue(x);
+            }
+
+            print.Print();
+            Console.WriteLine("First: " + print.First());
+        }
+    }
+}
